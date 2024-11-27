5ef7115b1e7c0faac3fee5aac20b71d2328720ff
Cabbage Stone <calebstone800@gmail.com>
2024-11-27 06:15:43 -0700

    rename the init script to make more sense
    erroneously named it init.db, but it should be a shell script.
    
    Additional changes in the new file:
    
    create the admin user instead of using superuser
    create a new database for the application

---
+++ docker/init-db.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# Create the admin user to prevent using the superuser for everything
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "${POSTGRES_DB}" <<-EOSQL
+    CREATE ROLE "${POSTGRES_ADMIN_USER}" WITH
+        CREATEDB
+        CREATEROLE
+        LOGIN
+        ENCRYPTED PASSWORD '${POSTGRES_ADMIN_PASSWORD}';
+EOSQL
+
+# Values must be quoted to preserve the case of username/password
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_ADMIN_USER" -d "${POSTGRES_DB}" <<-EOSQL
+    CREATE DATABASE "${POSTGRES_APP_DB}";
+    \c "${POSTGRES_APP_DB}";
+
+    CREATE USER "${POSTGRES_APP_USER}" WITH ENCRYPTED PASSWORD '${POSTGRES_APP_PASSWORD}';
+    GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_APP_DB} TO "${POSTGRES_APP_USER}";
+    GRANT ALL ON SCHEMA public TO "${POSTGRES_APP_USER}";
+    GRANT CREATE ON SCHEMA public TO "${POSTGRES_APP_USER}";
+    GRANT USAGE ON SCHEMA public TO "${POSTGRES_APP_USER}";
+EOSQL
